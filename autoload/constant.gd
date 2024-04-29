8c3806d4fc56c2f171da9d8133ca02c4af7b003e
Steven Coghill <scoghill1955@gmail.com>
2024-04-29 05:49:01 -0400

    add sounds

--- autoload/constant.gd
+++ autoload/constant.gd
@@ -3,8 +3,7 @@ extends Node
 
 const PICTURE_WIDTH := 1200
 const PICTURE_HEIGHT := 800
-const HFRAME_COUNT := 120
-const VFRAME_COUNT := 80
 const PATTERN_SIZE := 100
+#const PATTERN_SIZE := 100
 
 
